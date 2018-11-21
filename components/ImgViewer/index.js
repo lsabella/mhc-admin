e505df6114cdf2fe4234fa8718b1770b3bce5405
baishu <baishu@maihaoche.com>
2018-11-21 22:42:35 +0800

    index

--- components/ImgViewer/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Viewer from 'react-viewer';
-import 'react-viewer/dist/index.css';
-
-const ImgViewer = props => <Viewer {...props} />;
-
-export default ImgViewer;
--- site/theme/template/components/Layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default props => (
-  <footer>
-    <div className="footer-inner">
-    Made with <span className="heart">❤</span> by <a href="#" target="_blank">MHC-FE</a>
-    </div>
-  </footer>
-);
